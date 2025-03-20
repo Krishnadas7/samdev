788c7348b9b9b7f509e30882d381b6b133722aa0
krishnadas710 <skrishnadas61@gmail.com>
2025-03-20 11:03:18 +0530

    builded

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "./style.css";
-
-import './i-map.css'
+import './components/style.css'
+import './components/i-map.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
